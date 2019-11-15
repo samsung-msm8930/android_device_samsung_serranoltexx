d0a4bbd345d596c213341536533e563ed971533a
ripee <ripee.dev@gmail.com>
2019-11-15 11:16:03 +0100

    serranoltexx: Havoc-ify

--- havoc_serranoltexx.mk
+++ havoc_serranoltexx.mk
@@ -1,5 +1,5 @@
 # Copyright (C) 2013-2016, The CyanogenMod Project
-# Copyright (C) 2017, The LineageOS Project
+# Copyright (C) 2017, The Havoc-OS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,10 +19,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from serranoltexx device
 $(call inherit-product, device/samsung/serranoltexx/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Havoc-OS stuff.
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_serranoltexx
+PRODUCT_NAME := havoc_serranoltexx
 PRODUCT_DEVICE := serranoltexx
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
